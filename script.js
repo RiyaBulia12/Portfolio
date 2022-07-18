5f5f99840a30a708ebb62dc9940dd66384ce4a30
RiyaBulia12 <riyabulia12@gmail.com>
2022-07-18 22:55:06 +0530

    Add page location to each menu item

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('.menu-icon');
 const menuOverlay = document.querySelector('.menu-overlay');
+const menuOption = document.querySelectorAll('.menu-option li');
 const closeBtn = document.querySelector('.close-icon');
 
 if(menuBtn){
@@ -8,8 +9,20 @@ if(menuBtn){
    })
 }
 
+if(menuOption){
+   menuOption.forEach(element => {
+      element.addEventListener('click', ()=>{
+         closeNav();
+      })
+   });
+}
+
 if(closeBtn)  {
    closeBtn.addEventListener('click', () => {
-      menuOverlay.style.cssText = `width: 0%; display: none;`;
+      closeNav();
    })
+}
+
+function closeNav() {
+   menuOverlay.style.cssText = `width: 0%; display: none;`;
 }
