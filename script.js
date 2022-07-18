cac2ee93fb9737c6cde10bc8faac5e6043dedff3
RiyaBulia12 <riyabulia12@gmail.com>
2022-07-19 01:09:15 +0530

    Fix menu for desktop

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const menuBtn = document.querySelector('.menu-icon');
-const menuOverlay = document.querySelector('.menu-overlay');
-const menuOption = document.querySelectorAll('.menu-option li');
-const closeBtn = document.querySelector('.close-icon');
+if (screen.width <= 768) {
+   const menuBtn = document.querySelector('.menu-icon');
+   const menuOverlay = document.querySelector('.menu-overlay');
+   const menuOption = document.querySelectorAll('.menu-option li');
+   const closeBtn = document.querySelector('.close-icon');
 
-if(menuBtn){
-   menuBtn.addEventListener('click', () => {
-      menuOverlay.style.cssText = `width: 100%; display: block;`;
-      document.body.style.overflow = 'hidden';
-   })
-}
+   if(menuBtn){
+      menuBtn.addEventListener('click', () => {
+         menuOverlay.style.cssText = `width: 100%; display: block;`;
+         document.body.style.overflow = 'hidden';
+      })
+   }
 
-if(menuOption){
-   menuOption.forEach(element => {
-      element.addEventListener('click', ()=>{
-         document.body.style.overflow = 'auto';
+   if(menuOption){
+      menuOption.forEach(element => {
+         element.addEventListener('click', ()=>{
+            document.body.style.overflow = 'auto';
+            closeNav();
+         })
+      });
+   }
+
+   if(closeBtn)  {
+      closeBtn.addEventListener('click', () => {
          closeNav();
       })
-   });
-}
+   }
 
-if(closeBtn)  {
-   closeBtn.addEventListener('click', () => {
-      closeNav();
-   })
-}
-
-function closeNav() {
-   menuOverlay.style.cssText = `width: 0%; display: none;`;
+   function closeNav() {
+      menuOverlay.style.cssText = `width: 0%; display: none;`;
+   }
 }
