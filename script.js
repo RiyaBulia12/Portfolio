339ce9a5de03e61a8cc43b79ffb978ae1a9e1ec3
RiyaBulia12 <riyabulia12@gmail.com>
2022-07-19 01:13:13 +0530

    Fix ESLint errors

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-if (screen.width <= 768) {
-   const menuBtn = document.querySelector('.menu-icon');
-   const menuOverlay = document.querySelector('.menu-overlay');
-   const menuOption = document.querySelectorAll('.menu-option li');
-   const closeBtn = document.querySelector('.close-icon');
+const menuBtn = document.querySelector('.menu-icon');
+const menuOverlay = document.querySelector('.menu-overlay');
+const menuOption = document.querySelectorAll('.menu-option li');
+const closeBtn = document.querySelector('.close-icon');
 
+function closeNav() {
+   menuOverlay.style.cssText = `width: 0%; display: none;`;
+}
+
+if (screen.width <= 768) {
    if(menuBtn){
       menuBtn.addEventListener('click', () => {
          menuOverlay.style.cssText = `width: 100%; display: block;`;
          document.body.style.overflow = 'hidden';
       })
    }
-
    if(menuOption){
       menuOption.forEach(element => {
          element.addEventListener('click', ()=>{
@@ -19,14 +22,9 @@ if (screen.width <= 768) {
          })
       });
    }
-
    if(closeBtn)  {
       closeBtn.addEventListener('click', () => {
          closeNav();
       })
-   }
-
-   function closeNav() {
-      menuOverlay.style.cssText = `width: 0%; display: none;`;
    }
 }
