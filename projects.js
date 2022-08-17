f38f17965ef48320d613a73388144a6c425c4c95
Apple <riyabulia12@gmail.com>
2022-08-17 14:25:16 +0530

    Add project image, icons and modify style

--- projects.js
+++ projects.js
@@ -1,67 +1,10 @@
-const projects = [
-   {
-      id: 1,
-      title: 'Tonic',
-      info: {
-         client: 'canopy',
-         role: 'Back End Dev',
-         year: 2015,
-      },
-      screenshot: 'images/desktop-version/project-1.png',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
-      skills: ['html', 'css', 'javascript'],
-      liveLink: 'https://www.canopy.cr/tonic',
-      sourceLink: 'https://www.canopy.cr/tonic'
-   },
-   {
-      id: 2,
-      title: 'Multi-Post Stories',
-      info: {
-         client: 'Facebook',
-         role: 'Full Stack Dev',
-         year: 2015,
-      },
-      screenshot: 'images/desktop-version/project-2.png',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
-      skills: ['html', 'css', 'Ruby on rails', 'javascript'],
-      liveLink: 'https://www.facebook.com/',
-      sourceLink: 'https://www.facebook.com/'
-   },
-   {
-      id: 3,
-      title: 'Facebook 360',
-      info: {
-         client: 'Facebook',
-         role: 'Full Stack Dev',
-         year: 2015,
-      },
-      screenshot: 'images/desktop-version/project-3.png',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
-      skills: ['html', 'css', 'Ruby on rails', 'javascript'],
-      liveLink: 'https://www.facebook.com/',
-      sourceLink: 'https://www.facebook.com/'
-   },
-   {
-      id: 4,
-      title: 'Uber Navigation',
-      info: {
-         client: 'Uber',
-         role: 'Lead Developer',
-         year: 2018,
-      },
-      screenshot: 'images/desktop-version/project-4.png',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
-      skills: ['html', 'css', 'javascript', 'Ruby on rails'],
-      liveLink: 'https://www.uber.com/',
-      sourceLink: 'https://www.uber.com/'
-   }
-]
+import projects from './projectsData.js'
 
 document.querySelector('.portfolio-card').innerHTML = projects.map((project) =>
    `<div class="project-container">
       <div class="project-img-container">
          <a href="#" aria-label="Tonic Project">
-            <img src="images/project/tonic-project.png" alt="tonic-project" class="project-img">
+            <img src="${project.screenshot}" alt="tonic-project" class="project-img">
          </a>
       </div>
       <div class="about-project flex">
@@ -88,13 +31,17 @@ document.querySelector('.portfolio-card').innerHTML = projects.map((project) =>
             </ul>
          </div>
          <div class="btn-container">
-            <button type="submit" class="view-btn" data-bs-toggle="modal" data-bs-target="#projectModal" onclick="viewProject(${project.id})">See Project</button>
+            <button type="submit" class="view-btn" data-bs-toggle="modal" data-bs-target="#projectModal" id="${project.id}" >See Project</button>
          </div>
       </div>
    </div>`).join('');
 
 
-document.getElementsByClassName('.view-btn').onclick = function () { viewProject(this.id) };
+document.querySelectorAll('.view-btn').forEach((viewBtn) => {
+   viewBtn.onclick = (event) => {
+      viewProject(event.target.id)
+   };
+})
 
 function viewProject(id) {
    projects.map((project) => {
---
+++ projectsData.js
@@ -0,0 +1,60 @@
+const projects = [
+   {
+      id: 1,
+      title: 'Awesome Book App',
+      info: {
+         client: 'R.B.',
+         role: 'Front-End Dev',
+         year: 2022,
+      },
+      screenshot: 'images/desktop-version/awesome-book.png',
+      description: `Awesome books is a simple fully functional website that displays a list of books and allows you to add and remove books from that list. This application is built using Single Page Application concept where three links added on top of the page: List, Create and Contact. When the user clicks on any of the three links, the main area changes to display only one of the three content sections.`,
+      skills: ['html5', 'css3', 'javascript', 'es6'],
+      liveLink: 'https://riyabulia12.github.io/Awesome-Book/',
+      sourceLink: 'https://github.com/RiyaBulia12/Awesome-Book/'
+   },
+   {
+      id: 2,
+      title: 'To-Do List',
+      info: {
+         client: 'R.B.',
+         role: 'Full Stack Dev',
+         year: 2022,
+      },
+      screenshot: 'images/desktop-version/to-do-list.png',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
+      skills: ['html5', 'css3', 'javascript', 'es6', 'webpack'],
+      liveLink: 'https://riyabulia12.github.io/To-do-list/dist/',
+      sourceLink: 'https://github.com/RiyaBulia12/To-do-list'
+   },
+   {
+      id: 3,
+      title: 'Facebook 360',
+      info: {
+         client: 'Facebook',
+         role: 'Full Stack Dev',
+         year: 2015,
+      },
+      screenshot: 'images/desktop-version/project-3.png',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
+      skills: ['html', 'css', 'Ruby on rails', 'javascript'],
+      liveLink: 'https://www.facebook.com/',
+      sourceLink: 'https://www.facebook.com/'
+   },
+   {
+      id: 4,
+      title: 'Uber Navigation',
+      info: {
+         client: 'Uber',
+         role: 'Lead Developer',
+         year: 2018,
+      },
+      screenshot: 'images/desktop-version/project-4.png',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
+      skills: ['html', 'css', 'javascript', 'Ruby on rails'],
+      liveLink: 'https://www.uber.com/',
+      sourceLink: 'https://www.uber.com/'
+   }
+]
+
+export default projects;
