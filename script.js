dd8c1513dec7785c6bffe39e32bfb3236b433fbb
riyabulia12 <riyabulia12@gmail.com>
2023-02-15 22:45:41 +0530

    Add skill and social script

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const menuOverlay = document.querySelector('.menu-overlay');
 const menuOption = document.querySelectorAll('.menu-option li');
 const closeBtn = document.querySelector('.close-icon');
 const contactForm = document.getElementById('conatct-form');
+const navbar = document.querySelector(".navbar-scroll");
 
 function closeNav() {
    menuOverlay.style.cssText = `width: 0%; display: none;`;
 }
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
+});
 
 if (screen.width <= 768) {
    if (menuBtn) {
@@ -41,31 +50,3 @@ contactForm.addEventListener('click', function () {
       error.style.cssText = `visibility: hidden; opacity: 0`;
    }
 })
-
-document.querySelectorAll('.social-media-container').forEach((elem) => {
-   elem.innerHTML = `
-<h2 class="social-media-title">Let's Connect</h2>
-            <ul class="social-media-content flex">
-               <li>
-                  <a href="https://www.linkedin.com/in/riya-bulia/" aria-label="LinkedIn Icon">
-                     <img class="social-media-icons" src="images/icons/linkedin.png" alt="LinkedIn Icon">
-                  </a>
-               </li>
-               <li>
-                  <a href="mailto:riyabulia12@gmail.com" aria-label="Gmail Icon">
-                     <img class="social-media-icons" src="images/icons/gmail.png" alt="Gmail Icon">
-                  </a>
-               </li>
-               <li>
-                  <a href="https://github.com/RiyaBulia12" aria-label="Github Icon">
-                     <img class="social-media-icons" src="images/icons/github.png" alt="Github Icon">
-                  </a>
-               </li>
-               <li>
-                  <a href="https://leetcode.com/RiyaBulia/" aria-label="Leetcode Icon">
-                     <img class="social-media-icons" src="images/icons/leetcode.png" alt="Leetcode Icon">
-                  </a>
-               </li>
-            </ul>
-            `
-});
